fc29d15d412773d89de31e9620f1b80d4fd12b3e
diegocgcfad <diegocgcfad@hotmail.com>
2025-01-08 19:48:48 -0300

    Projeto pronto

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const inputWrapper = document.querySelectorAll('.input-wrapper');
+const fields = document.querySelectorAll('.field');
+const send = document.querySelector('.send');
+const form = document.querySelector('.form');
+
+form.onsubmit = function() {
+    return false;
+}
+
+send.addEventListener('click', function() {
+    fields.forEach(function(item, index){
+        if(item.value == ""){
+            inputWrapper[index].classList.add('error');
+            item.classList.add('border-error');
+        }else{
+            inputWrapper[index].classList.remove('error');
+            item.classList.remove('border-error');
+            item.classList.add('border-ok'); 
+        }
+    })
+});
